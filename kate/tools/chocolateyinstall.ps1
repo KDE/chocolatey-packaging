0652306dc481e1bef484294a6a61870a24451b9c
Gleb Popov <6yearold@gmail.com>
2021-10-08 11:24:21 +0300

    Update Kate to 21.08.2 as well as scripts for building the package.
    Scripts were taken from https://gitlab.com/dimqua/choco/-/tree/stable/kate

--- kate/tools/chocolateyinstall.ps1
+++ kate/tools/chocolateyinstall.ps1
@@ -1,30 +1,12 @@
-$version = "17.08.1-KF5.38"
-$url32       = "https://download.kde.org/stable/kate/Kate-setup-$version-32bit.exe"
-$url64       = "https://download.kde.org/stable/kate/Kate-setup-$version-64bit.exe"
-$checksum32  = "33a0f2600feeaf5d9f1bb9edb0ddb492615d7fc375877b0c41de8b9e2a3b6f93"
-$checksum64  = "9ff450dc338cc4cae1011dbe99969c3fcd5e85a159198dd8616caadfc2ac8950"
+﻿$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$installArgs  = "/S"
-
-$params = @{
-  packageName    = "kate"
-  fileType       = "EXE"
-  silentArgs     = $installArgs
-  url            = $url32
-  url64Bit       = $url64
-  checksum       = $checksum32
-  checksum64     = $checksum64
-  checksumType   = "sha256"
-  checksumType64 = "sha256"
+$packageArgs = @{
+  packageName = $env:ChocolateyPackageName
+  file64      = "$toolsDir\kate-21.08.2-1433-windows-msvc2019_64-cl.exe"
+  destination = $toolsDir
+  silentArgs  = "/S"
 }
 
-$ROOT=[System.IO.Path]::GetDirectoryName($myInvocation.MyCommand.Definition)
-& "$ROOT/chocolateyUninstall.ps1"
+Install-ChocolateyInstallPackage @packageArgs
 
-Install-ChocolateyPackage @params
-
-[array]$key = Get-UninstallRegistryKey -SoftwareName "Kate*"
-if ($key -ne $null) {
-	$instDir = Split-Path -Parent $key.UninstallString
-	Install-BinFile -Name "kate" -Path "$instDir/bin/kate.exe"
-}
+Remove-Item $toolsDir\*.exe -ea 0
