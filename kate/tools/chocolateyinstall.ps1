27d1bf691a044d35e8000ef457e031886c77c1f3
Gleb Popov <6yearold@gmail.com>
2022-09-20 13:23:10 +0300

    Update Kate to 22.08.1

--- kate/tools/chocolateyinstall.ps1
+++ kate/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
-  file64      = "$toolsDir\kate-22.08.0-1762-windows-msvc2019_64-cl.exe"
+  file64      = "$toolsDir\kate-22.08.1-1789-windows-msvc2019_64-cl.exe"
   destination = $toolsDir
   silentArgs  = "/S"
 }
